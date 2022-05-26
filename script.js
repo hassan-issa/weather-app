bd4d23ff2843c0033c13c49538369ab3a225d680
Hassan Issa <hassan.issa2@outlook.com>
2022-05-26 17:03:15 -0400

    completed display function to console

--- script.js
+++ script.js
@@ -2,21 +2,24 @@ let weather = {
     apiKey: "e89a1ba9d6a20812f1d834480b38f6c0",
     fetchWeather: function (city) {
         fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" +
-                city +
-                "&units=metric&appid=" +
-                this.apiKey
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            +"&appid=" 
+            + this.apiKey + "&units=metric"
         )
-            .then((response) => response.json())
-            .then((data) => this.displayWeather(data));
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data))
     },
-    displayWeather: function(data) {
+    displayWeather: function (data) {
         const { name } = data;
-        const { icon, description} = data.weather[0];
-        const { temp, humidity } = data.main;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity} = data.main;
         const { speed } = data.wind;
-        console.log(name,icon,description,temp,humidity,speed);
-        document.querySelector(".city").innerText = "Weather in " + name;
-    }
+        document.querySelector(".city").innerHTML = "Weather in " + name;
+        document.querySelector(".temp").innerHTML = temp + "°C";
+        document.querySelector(".description").innerHTML = description;
+        document.querySelector(".humidity").innerHTML = "Humidity: " + humidity; + "%";
+        document.querySelector(".wind").innerHTML = "Wind speed: " + speed + " km/h";
+    },
 };
 
